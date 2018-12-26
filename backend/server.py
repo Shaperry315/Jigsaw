6339500afe5b5c11d6bef7f0dc0b2c7ad1e13adf
shaperry315 <shaperry315@gmail.com>
2018-12-25 23:56:34 -0500

    Second commit, app routes in place, auth in the works

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,33 @@
 from flask import Flask
 from flask import render_template
+from decorator import requires_auth
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<name>")
-def hello(name=None):
-	return render_template('index.html', name=name)
+@requires_auth
+def index_page():
+	return render_template('index.html')
+
+@app.route("/bsignup")
+def bsignup_page():
+	return decorator.hello()
+
+@app.route("/bsignin")
+def bsignin_page():
+	return render_template('bsignin.html')
+
+@app.route("/jsignup")
+def jsignup_page():
+	return render_template('jsignup.html')
+
+@app.route("/jsignin")
+def jsignin_page():
+	return render_template('jsignin.html')
+
+@app.route("/employeelist")
+def employeelist_page():
+	return render_template('employeelist.html')
+
+@app.route("/feedback")
+def feedback_page():
+	return render_template('feedback.html')
